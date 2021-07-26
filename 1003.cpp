d447ee9fc71ea9017e02351e43b32837e1295c71
otifik <3078496627@qq.com>
2021-07-26 11:35:21 +0800

    重新修改p1003

--- 1003.cpp
+++ 1003.cpp
@@ -1,54 +1,51 @@
 #include<iostream>
-#include<algorithm>
 #include<string.h>
 using namespace std;
 #define inf 0x3f3f3f3f
 int n,m,c1,c2;
-//dis[i]表示从出发点到i结点最短路径的路径长度，num[i]表示从出发点到i结点最短路路径的条数，w[i]表示从出发点到i点救援队的数⽬目之和
-int e[510][510],weight[510],dis[510],num[510],w[510];
-int vis[510];
+int num[1010],e[1010][1010],weight[1010],w[1010],dis[1010],vis[1010];
 int main(){
     cin>>n>>m>>c1>>c2;
+    memset(e,inf,sizeof(e));
+    memset(dis,inf,sizeof(dis));
     for(int i=0;i<n;i++){
         cin>>weight[i];
     }
-    memset(e,inf,sizeof(e));
-    int a,b,c;
     for(int i=0;i<m;i++){
-        cin>>a>>b>>c;
-        e[a][b]=e[b][a]=c;
+        int u,v,w;
+        cin>>u>>v>>w;
+        e[u][v]=e[v][u]=w;
     }
-    for(int i=0;i<n;i++){
-        e[i][i]=0;
-    }
-    for(int i=0;i<n;i++){
-        dis[i]=e[c1][i];
-    }
+    dis[c1]=0;
     num[c1]=1;
     w[c1]=weight[c1];
     for(int i=0;i<n;i++){
-        int min=inf,index;
-        for(int j=0;j<n-1;j++){
+        int u=-1,min=inf;
+        for(int j=0;j<n;j++){
             if(!vis[j]&&dis[j]<min){
+                u=j;
                 min=dis[j];
-                index=j;
             }
         }
-        vis[index]=1;
-        for(int j=0;j<n;j++){
-            if(!vis[j]&&e[index][j]!=inf&&e[index][j]!=0){
-                if(e[index][j]+dis[index]<dis[j]){
-                    dis[j]=e[index][j]+dis[index];
-                    num[j]=num[index];
-                    w[j]=w[index]+weight[j];
-                }else if(e[index][j]+dis[index]==dis[j]){
-                    num[j]+=num[index];
-                    if(w[index]+weight[j]>w[j]){
-                        w[j]=w[index]+weight[j];
+        if(u==-1){
+            break;
+        }
+        vis[u]=1;
+        for(int v=0;v<n;v++){
+            if(!vis[v]&&e[u][v]!=inf){
+                if(dis[u]+e[u][v]<dis[v]){
+                    num[v]=num[u];
+                    w[v]=w[u]+weight[v];
+                    dis[v]=dis[u]+e[u][v];
+                }else if(dis[u]+e[u][v]==dis[v]){
+                    num[v]+=num[u];
+                    if(w[u]+weight[v]>w[v]){
+                        w[v]=w[u]+weight[v];
                     }
                 }
             }
         }
     }
     cout<<num[c2]<<" "<<w[c2];
+    return 0;
 }
--- Algorithm Template/Dijkstra.cpp
+++ Algorithm Template/Dijkstra.cpp
@@ -1,108 +1,32 @@
 #include<iostream>
 #include<string.h>
-using namespace std;
 #define inf 0x3f3f3f3f
-int map[1010][1010];//这是一个二维模拟数组，map[i][j]对应从i到j的边权（注意不要和边长混淆概念）
-int vis[1010],dis[1010];//vis[i]代表i到某一点是否是确定的最短路径，dis[i]是确定的最短路径的值
-int n,m;//n代表有多少点，m代表有多少边
-int start;//代表起点
-//本模板点下标从1到n
-int main(){
-    cin>>n>>m;
-    //初始化很重要，不要忘记
-    memset(map,inf,sizeof(map));
-    for(int i=1;i<=n;i++){
-        map[i][i]=0;
-    }
-    //读入图
-    for(int i=0;i<m;i++){
-        int u,v,w;
-        cin>>u>>v>>w;
-        map[u][v]=w;
-        map[v][u]=w;
-    }
-    /*以下为Dijkstra实现（可抽取为一个函数）*/
-    //vis数组一开始就是定义在main函数外，所以不需要memset，本身就全是0
-    //接下来先初始化dis数组
-    cin>>start;
-    for(int i=1;i<=n;i++){
-        dis[i]=map[start][i];
-    }
-    vis[start]=1;//因为是初始点，所以直接标记为访问过
-    for(int i=1;i<n;i++){//为了确保每个点到起始点都是最短路径，就要进行n-1次的迭代，使得vis数组从1到n都是1，即都访问过
-        //下面的操作是找出未确定最短路径的点中离初始点最近的一个点
-        int min=inf,index;
-        for(int j=1;j<=n;j++){
-            if(!vis[j]&&dis[j]<min){
-                min=dis[j];
-                index=j;
-            }
-        }
-        vis[index]=1;//表明已经确定最短路径
-        //下面的操作就是逐个更新最段路
-        for(int j=1;j<=n;j++){
-            if(map[index][j]+dis[index]<dis[j]){
-                dis[j]=map[index][j]+dis[index];
-            }
-        }
-    }
-    /*至此，Dijkstra实现结束*/
-    //这边就进行一下简单的输出
-    for(int i=1;i<=n;i++){
-        cout<<dis[i]<<" ";
-    }
-    return 0;
-}
-
-#include<iostream>
-#include<string.h>
+#define maxn 1010
 using namespace std;
-#define inf 0x3f3f3f3f
-int map[1010][1010];//这是一个二维模拟数组，map[i][j]对应从i到j的边权（注意不要和边长混淆概念）
-int vis[1010],dis[1010];//vis[i]代表i到某一点是否是确定的最短路径，dis[i]是确定的最短路径的值
-int n,m;//n代表有多少点，m代表有多少边
-int start;//代表起点
-//本模板点下标从1到n
+int e[maxn][maxn],vis[maxn],dis[maxn];
+int n,m,start,dest;
 int main(){
-    cin>>n>>m;
-    //初始化很重要，不要忘记
-    memset(map,inf,sizeof(map));
-    //读入图
-    for(int i=0;i<m;i++){
-        int u,v,w;
-        cin>>u>>v>>w;
-        map[u][v]=w;
-        map[v][u]=w;
-    }
-    /*以下为Dijkstra实现（可抽取为一个函数）*/
-    //vis数组一开始就是定义在main函数外，所以不需要memset，本身就全是0
-    cin>>start;
+    //输入，略
+    memset(e,inf,sizeof(e));
     memset(dis,inf,sizeof(dis));
     dis[start]=0;
-    for(int i=1;i<=n;i++){//为了确保每个点到起始点都是最短路径，就要进行n次的迭代，使得vis数组从1到n都是1，即都访问过
-        //下面的操作是找出未确定最短路径的点中离初始点最近的一个点
-        int min=inf,index=-1;
-        for(int j=1;j<=n;j++){
+    for(int i=0;i<n;i++){
+        int u=-1,min=inf;
+        for(int j=0;j<n;j++){
             if(!vis[j]&&dis[j]<min){
+                u=j;
                 min=dis[j];
-                index=j;
             }
         }
-        if(index==-1){
+        if(u==-1){
             break;
         }
-        vis[index]=1;//表明已经确定最短路径
-        //下面的操作就是逐个更新最段路
-        for(int j=1;j<=n;j++){
-            if(map[index][j]+dis[index]<dis[j]){
-                dis[j]=map[index][j]+dis[index];
+        vis[u]=1;
+        for(int v=0;v<n;v++){
+            if(!vis[v]&&e[u][v]!=inf&&dis[u]+e[u][v]<dis[v]){
+                dis[v]=dis[u]+e[u][v];
             }
         }
-    }
-    /*至此，Dijkstra实现结束*/
-    //这边就进行一下简单的输出
-    for(int i=1;i<=n;i++){
-        cout<<dis[i]<<" ";
     }
     return 0;
 }
