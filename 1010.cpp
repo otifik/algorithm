62b78a276ce381af83ff7810b0f3b2beac856549
otifik <3078496627@qq.com>
2021-07-28 11:07:13 +0800

    未完成的1010 & 模板

---
+++ 1010.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+long long convert(string n, long long radix) {
+    long long sum = 0;
+    int index = 0, temp = 0;
+    for (auto it = n.rbegin(); it != n.rend(); it++) {
+        temp = isdigit(*it) ? *it - '0' : *it - 'a' + 10;
+        sum += temp * pow(radix, index++);
+    }
+    return sum;
+}
+long long find_radix(string n, long long num) {
+    char it = *max_element(n.begin(), n.end());
+    long long low = (isdigit(it) ? it - '0': it - 'a' + 10) + 1;
+    long long high = max(num, low);
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long t = convert(n, mid);
+        if (t < 0 || t > num) high = mid - 1;
+        else if (t == num) return mid;
+        else low = mid + 1;
+    }
+    return -1;
+}
+int main() {
+    string n1, n2;
+    long long tag = 0, radix = 0, result_radix;
+    cin >> n1 >> n2 >> tag >> radix;
+    result_radix = tag == 1 ? find_radix(n2, convert(n1, radix)) : find_radix(n1, convert(n2, radix));
+    if (result_radix != -1) {
+        printf("%lld", result_radix);
+    } else {
+        printf("Impossible");
+    }   
+    return 0;
+}
+
+/*
+    大致思路：把给出基数的数字转化为十进制，再处理另一个数字，多少进制转化为十进制数等于之前被转化的那个数字
+
+*/
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+string n1,n2;
+long long tag,radix;
+long long convert(string s,long long radix){
+    long long sum = 0;
+    int index = 0,temp = 0;
+    for(auto it = s.rbegin();it!=s.rend();it++){
+        temp = isdigit(*it)?*it-'0':*it-'a'+10;
+        sum+=temp*pow(radix,index++);
+    }
+}
+/*
+long long find_radix(string n, long long num) {
+    char it = *max_element(n.begin(), n.end());
+    long long low = (isdigit(it) ? it - '0': it - 'a' + 10) + 1;
+    long long high = max(num, low);
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long t = convert(n, mid);
+        if (t < 0 || t > num) high = mid - 1;
+        else if (t == num) return mid;
+        else low = mid + 1;
+    }
+    return -1;
+}
+*/
+int main(){
+    cin>>n1>>n2>>tag>>radix;
+    return 0;
+}
