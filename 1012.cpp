fb36ad772433c31560861c40fa838d1b54dbe236
otifik <3078496627@qq.com>
2021-07-29 12:18:45 +0800

    half-complete

---
+++ 1012.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+char grade[4]={'A','C','M','E'};
+struct node {
+    int id,best;
+    int score[4],rank[4];
+    //ACME
+}stu[2100];
+int main(){
+
+    return 0;
+}
+
