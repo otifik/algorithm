0aaf7e5f6a9ba76cab2c389dd65a90b85da41ef9
otifik <3078496627@qq.com>
2021-08-01 09:37:00 +0800

    modify pta advance 1015

--- 1015.cpp
+++ 1015.cpp
@@ -34,7 +34,6 @@ int main(){
         for(int i=0;i<cnt;i++){
             reverseNum=r*reverseNum+a[i];
         }
-        cout<<reverseNum<<endl;
         if(prime(n)&&prime(reverseNum)){
             cout<<"Yes"<<endl;
         }else {
