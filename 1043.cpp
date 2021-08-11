62594494a3ade3c8d835361501372c7d819c502b
otifik <3078496627@qq.com>
2021-08-11 10:48:49 +0800

    update 1043

--- 1043.cpp
+++ 1043.cpp
@@ -78,3 +78,53 @@ int main(){
     }
     return 0;
 }
+
+/*柳神代码，只需要根据二叉树性质左子树小于root，右子树大于root，和前序遍历，即可推导出后序遍历
+如果后序遍历出来的节点个数不等于总节点个数，说明这不是二叉搜索树
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> pre,post;
+bool isMirror=false;
+int n;
+void getPost(int root,int tail){
+    if(root>tail) return;
+    int i=root+1,j=tail;
+    if(isMirror){
+        while(i<=tail&&pre[root]<=pre[i]) i++;
+        while(j>=root+1&&pre[root]>pre[j]) j--;
+    }else {
+        while(i<=tail&&pre[root]>pre[i]) i++;
+        while(j>=root+1&&pre[root]<=pre[j]) j--;
+    }
+    if(i-j!=1){
+        return;
+    }
+    getPost(root+1,j);
+    getPost(i,tail);
+    post.push_back(pre[root]);
+}
+int main(){
+    cin>>n;
+    pre.resize(n);
+    for(int i=0;i<n;i++){
+        cin>>pre[i];
+    }
+    getPost(0,n-1);
+    if(post.size()!=n){
+        post.clear();
+        isMirror=true;
+        getPost(0,n-1);
+    }
+    if(post.size()==n){
+        cout<<"YES"<<endl;
+        for(int i=0;i<n;i++){
+            if(i){
+                cout<<" ";
+            }
+            cout<<post[i];
+        }
+    }else cout<<"NO";
+    return 0;
+}
